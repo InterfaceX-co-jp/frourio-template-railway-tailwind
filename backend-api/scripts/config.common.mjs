4fdb8d15f3ad1016c60784ffe7ac3f1c808aeb0f
mikana0918 <shogo.kusuhara@gmail.com>
2024-11-16 16:55:40 +0900

    refactor: Update path resolution in config.common.mjs to use dynamic import.meta.url

--- backend-api/scripts/config.common.mjs
+++ backend-api/scripts/config.common.mjs
@@ -1,9 +1,13 @@
 import { nodeExternalsPlugin } from 'esbuild-node-externals';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 export default {
-  entryPoints: [path.resolve(__dirname, '../entrypoints/index.ts')],
-  outdir: path.resolve(__dirname, '../'),
+  entryPoints: [path.resolve(dirname, '../entrypoints/index.ts')],
+  outdir: path.resolve(dirname, '../'),
   platform: 'node',
   target: 'node20',
   bundle: true,
